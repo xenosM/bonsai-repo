ac8560d427929df4052e304ae8431c1221ffa489
xenosM <105599352+xenosM@users.noreply.github.com>
2024-07-24 12:05:10 +0545

    product section COMPLETE

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const workflow= document.querySelector('.header__dropdown-workflow');
     const rightSide= document.querySelector('.header__dropdown-right');
-    // const leftSide= document.querySelector('.header__dropdown-left');
     const dropdownContent= document.querySelector('.header__dropdown-content');
+    const leftsideElement= document.querySelector('.leftside__element');
+    //hover on workflow element to show right side
     workflow.addEventListener('mouseover',()=>{
-        rightSide.style.display = 'block';
-        rightsdie
-        rightSide.style.transition = 'all 1s';
+        rightSide.style.width='auto'
+        rightSide.style.visibility = 'visible';
+        rightSide.style.opacity= 1;
 
     })
+
+    //hover out to hide right side
     dropdownContent.addEventListener('mouseleave',()=>{
-        rightSide.style.display = 'none';
+        rightSide.style.width= 0;
+        rightSide.style.visibility=' hidden';
+        rightSide.style.opacity= 0;
     })
- 
+    leftsideElement.addEventListener('mouseover',()=>{
+        rightSide.style.width= 0;
+        rightSide.style.visibility=' hidden';
+        rightSide.style.opacity= 0;
     })
+})
